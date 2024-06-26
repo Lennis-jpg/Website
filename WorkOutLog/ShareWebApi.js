2b49fbf59f6fbc113a9b1dd8506fdd1c4b45dee5
Demos <69394991+Lennis-jpg@users.noreply.github.com>
2024-06-26 18:18:46 +0100

    Share Web Api

---
+++ WorkOutLog/ShareWebApi.js
@@ -0,0 +1,18 @@
+// Share Web Api 
+const share = document.getElementById('share')
+
+share.addEventListener('click', event => {
+
+  // Check for Web Share api support
+  if (navigator.share) {
+    // Browser supports native share api 
+    navigator.share({
+      text: 'WorkLog a easy and useful way of tracking workout sessions: ',
+      url: 'https://clerusync.com/WorkOutLog/'
+    });
+  } else {
+    // Alert is function not supported on browser
+    alert("This browser does not support the share function. Please share QR link instead")
+  }
+});
+// Share Web Api
