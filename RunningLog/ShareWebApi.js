245434c8644d3dea04915ae0b5ad0ba1a2b00d9c
Demos <69394991+Lennis-jpg@users.noreply.github.com>
2024-06-27 14:53:05 +0100

    Update ShareWebApi.js

--- RunningLog/ShareWebApi.js
+++ RunningLog/ShareWebApi.js
@@ -12,7 +12,7 @@ share.addEventListener('click', event => {
     });
   } else {
     // Alert is function not supported on browser
-    alert("This browser does not support the share function, please share QR code instead.")
+    alert("This browser does not support the share function, please share the QR code instead.")
   }
 });
 // Share Web Api
