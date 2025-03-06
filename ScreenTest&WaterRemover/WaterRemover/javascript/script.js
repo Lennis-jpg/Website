ef26b53f683683c4ccf6ab5bdef8f0ec5b39d758
Demos <69394991+Lennis-jpg@users.noreply.github.com>
2025-03-06 10:42:25 +0000

    assets, css and javascript folders

---
+++ ScreenTest&WaterRemover/WaterRemover/javascript/script.js
@@ -0,0 +1,18 @@
+//5 Minutes
+var sound1 = new Audio();
+sound1.src = "audio/5_Minutes.mp3";
+// 10 Minutes
+var sound1 = new Audio();
+sound1.src = "audio/10_Minutes.mp3";
+// 15 Minutes
+var sound1 = new Audio();
+sound1.src = "audio/15_Minutes.mp3";
+// 20 Minutes
+var sound1 = new Audio();
+sound1.src = "audio/20_Minutes.mp3";
+//25 Minutes
+var sound1 = new Audio();
+sound1.src = "audio/25_Minutes.mp3";
+//30 Minutes
+var sound1 = new Audio();
+sound1.src = "audio/30_Minutes.mp3";
