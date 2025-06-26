c88b1115bea9300df99c202c470983900b19ac45
Demos <69394991+Lennis-jpg@users.noreply.github.com>
2025-06-27 00:34:43 +0100

    Update main.js
    Added JavaScript for the cookie consent banner that appears on the index page. If accepted, it will last one year. If declined, it will reappear on refresh or the next time the website user visits the index page again.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,31 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
+
+/**
+* Cookie consent banner
+*/
+const cookieBox = document.querySelector(".wrapper"),
+  buttons = document.querySelectorAll(".button");
+
+const executeCodes = () => { 
+  if (document.cookie.includes("cookie")) return;
+  cookieBox.classList.add("show");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      cookieBox.classList.remove("show");
+ 
+      if (button.id == "acceptBtn") { 
+        //365 days
+        document.cookie = "cookieBy= cookie; max-age=" + 365;
+      }
+    });
+  });
+};
+ 
+window.addEventListener("load", executeCodes);
+
 /**
 * Popup form for the sell button
 */
