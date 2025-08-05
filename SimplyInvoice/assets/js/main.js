00ffaae19b45940620f861bf76127ead2a957fc7
Demos <69394991+Lennis-jpg@users.noreply.github.com>
2025-08-05 13:11:13 +0100

    SimplyInvoice Website

---
+++ SimplyInvoice/assets/js/main.js
@@ -0,0 +1,4 @@
+const iframe = document.createElement('iframe');
+    iframe.className = 'fullscreen-iframe';
+    iframe.src = 'https://simply-invoice-webserver.vercel.app'; 
+    document.body.appendChild(iframe);
