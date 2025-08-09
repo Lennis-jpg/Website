3d447a02f41816bb604460a5b9f85777aedda649
Demos <69394991+Lennis-jpg@users.noreply.github.com>
2025-08-09 14:19:33 +0100

    Delete To Invoice directory

--- To Invoice/assets/js/main.js
+++
@@ -1,4 +0,0 @@
-const iframe = document.createElement('iframe');
-    iframe.className = 'fullscreen-iframe';
-    iframe.src = 'https://simply-invoice-webserver.vercel.app'; 
-    document.body.appendChild(iframe);
