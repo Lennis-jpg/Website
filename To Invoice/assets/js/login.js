dbca40dd788a412e1def10930a690f753fef52d7
Demos <69394991+Lennis-jpg@users.noreply.github.com>
2025-08-09 14:18:38 +0100

    To Invoice Website

---
+++ To Invoice/assets/js/login.js
@@ -0,0 +1,4 @@
+const iframe = document.createElement('iframe');
+    iframe.className = 'fullscreen-iframe';
+    iframe.src = 'https://simply-invoice-webserver.vercel.app/Login.html'; 
+    document.body.appendChild(iframe);
