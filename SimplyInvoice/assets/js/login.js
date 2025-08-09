16794c7cb69b9f7256b98b0c97a09bbf4c74a4d1
Demos <69394991+Lennis-jpg@users.noreply.github.com>
2025-08-09 14:19:10 +0100

    Delete SimplyInvoice directory

--- SimplyInvoice/assets/js/login.js
+++
@@ -1,4 +0,0 @@
-const iframe = document.createElement('iframe');
-    iframe.className = 'fullscreen-iframe';
-    iframe.src = 'https://simply-invoice-webserver.vercel.app/Login.html'; 
-    document.body.appendChild(iframe);
