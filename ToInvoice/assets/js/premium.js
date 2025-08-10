d5885f778ff27fee6032fd2bbd409f69d353be52
Demos <69394991+Lennis-jpg@users.noreply.github.com>
2025-08-10 12:27:57 +0100

    Delete ToInvoice directory

--- ToInvoice/assets/js/premium.js
+++
@@ -1,4 +0,0 @@
-const iframe = document.createElement('iframe');
-    iframe.className = 'fullscreen-iframe';
-    iframe.src = 'https://simply-invoice-login-premium.vercel.app/Dashboard.html'; 
-    document.body.appendChild(iframe);
--- ToInvoice/assets/js/signup.js
+++
@@ -1,4 +0,0 @@
-const iframe = document.createElement('iframe');
-    iframe.className = 'fullscreen-iframe';
-    iframe.src = 'https://simply-invoice-webserver.vercel.app/Signup.html'; 
-    document.body.appendChild(iframe);
