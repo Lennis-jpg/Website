7ceeef8bee5f33ca6cee8ac0b74e20b709081575
Demos <69394991+Lennis-jpg@users.noreply.github.com>
2025-10-30 00:43:49 +0000

    Clerusync Investment website
    Uploaded the Clerusync Investment website

---
+++ ClerusyncInvestment/assets/js/login.js
@@ -0,0 +1,5 @@
+const iframe = document.createElement('iframe');
+    iframe.className = 'fullscreen-iframe';
+    iframe.src = 'https://clerusync-investment.vercel.app/login'; 
+    document.body.appendChild(iframe);
+    
---
+++ ClerusyncInvestment/assets/js/signup.js
@@ -0,0 +1,4 @@
+const iframe = document.createElement('iframe');
+    iframe.className = 'fullscreen-iframe';
+    iframe.src = 'https://clerusync-investment.vercel.app/signup'; 
+    document.body.appendChild(iframe);
