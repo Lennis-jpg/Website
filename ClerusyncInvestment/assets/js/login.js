0cad86321a6e78bcb20622d7fbfdfc159433b0b3
Demos <69394991+Lennis-jpg@users.noreply.github.com>
2026-03-08 16:01:36 +0000

    Update URL for login page

--- ClerusyncInvestment/assets/js/login.js
+++ ClerusyncInvestment/assets/js/login.js
@@ -1,5 +1,5 @@
 const iframe = document.createElement('iframe');
     iframe.className = 'fullscreen-iframe';
-    iframe.src = 'https://invoicly.co.uk/login'; 
+    iframe.src = 'https://clerusync-investment-new-login.vercel.app/login';  
     document.body.appendChild(iframe);
     
