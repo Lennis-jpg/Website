74a76b67be31c4e4f1bfcd045e3c0c3d35e5a0d8
Clerusync <69394991+clerusync@users.noreply.github.com>
2026-07-02 11:02:55 +0100

    Update starter.js
    Updated iframe source URL in the starter.js

--- sshie/assets/js/starter.js
+++ sshie/assets/js/starter.js
@@ -1,4 +1,4 @@
 const iframe = document.createElement('iframe');
     iframe.className = 'fullscreen-iframe';
-    iframe.src = 'https://sshie-starter.onrender.com'; 
+    iframe.src = 'https://sshiestart.clerusyncinvestment.com'; 
     document.body.appendChild(iframe);
